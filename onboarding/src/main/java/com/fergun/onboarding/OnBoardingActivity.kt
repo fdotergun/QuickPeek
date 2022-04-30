5b8dcdf3b352fe03a6cc3a8c30a5253924574ef1
ferhatergun <h.f.ergun@gmail.com>
2022-04-30 13:48:55 +0300

    re-creating hilt infrastructure

--- onboarding/src/main/java/com/fergun/onboarding/OnBoardingActivity.kt
+++ onboarding/src/main/java/com/fergun/onboarding/OnBoardingActivity.kt
@@ -2,14 +2,11 @@ package com.fergun.onboarding
 
 import androidx.activity.viewModels
 import com.fergun.core.ui.BaseActivity
-import dagger.hilt.android.AndroidEntryPoint
 
-@AndroidEntryPoint
 class OnBoardingActivity: BaseActivity() {
 
     val onBoardingViewModel: OnBoardingViewModel by viewModels()
     override val layoutId: Int
         get() = R.layout.activity_onboarding
-
 
 }
--- onboarding/src/main/java/com/fergun/onboarding/OnBoardingViewModel.kt
+++ onboarding/src/main/java/com/fergun/onboarding/OnBoardingViewModel.kt
@@ -1,14 +1,13 @@
 package com.fergun.onboarding
 
 import androidx.lifecycle.MutableLiveData
-import com.fergun.core.DummyInjection
 import com.fergun.core.ui.BaseViewModel
 import com.fergun.onboarding.model.SignUpModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class OnBoardingViewModel @Inject constructor(val dummyInjection: DummyInjection): BaseViewModel(){
+class OnBoardingViewModel @Inject constructor(): BaseViewModel(){
 
     private val _signUpModel = MutableLiveData<SignUpModel>()
     val signUpModel = _signUpModel
